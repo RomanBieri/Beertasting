8159b26eb705d4be0ca5b846b9fdfd4b276099ac
RomanBieri <roman.bieri@edu.teko.ch>
2025-09-17 19:19:58 +0200

    Refactor project structure for Vercel conventions

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Erfolgreich mit MongoDB verbunden");
+  } catch (err) {
+    console.error("Verbindungsfehler:", err.message);
+    process.exit(1);
+  }
+};
+connectDB();
+
+// KORREKTUR: Pfade sind jetzt relativ zum 'api'-Ordner
+app.use("/auth", require("./routes/auth.js"));
+app.use("/tasting", require("./routes/tasting.js"));
+
+module.exports = app;
