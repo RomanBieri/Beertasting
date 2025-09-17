62382102b8018c8d439f65e454173d38f541ee7e
RomanBieri <roman.bieri@edu.teko.ch>
2025-09-17 19:35:55 +0200

    Final Vercel configuration

--- api/index.js
+++ api/index.js
@@ -12,15 +12,12 @@ app.use(express.json());
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Erfolgreich mit MongoDB verbunden');
   } catch (err) {
     console.error('Verbindungsfehler:', err.message);
-    process.exit(1);
   }
 };
 connectDB();
 
-// Diese Pfade sind relativ zu 'index.js' und immer noch korrekt.
 app.use('/auth', require('./routes/auth.js'));
 app.use('/tasting', require('./routes/tasting.js'));
 
